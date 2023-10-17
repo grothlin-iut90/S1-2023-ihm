021ed88dd19ed3023a77e06bd07ee832ba676c0d
grothlin-iut90 <146071993+grothlin-iut90@users.noreply.github.com>
2023-10-17 20:27:44 +0200

    Update script.js

--- Gaël_R/script.js
+++ Gaël_R/script.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     var fadeIns = document.querySelectorAll('.fade-in');
-  
+    fadeIns.forEach(function(fadeIn) {
+      if (isElementInViewport(fadeIn)) {
+        fadeIn.style.opacity = 1;
+      }     
+    })
     window.addEventListener('scroll', function() {
       fadeIns.forEach(function(fadeIn) {
         if (isElementInViewport(fadeIn)) {
           fadeIn.style.opacity = 1;
+        }else{
+          fadeIn.style.opacity = 0;
         }
       });
     });
